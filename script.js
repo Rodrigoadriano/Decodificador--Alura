91de96c68f8119a9033945268b3cd84722bde2af
Rodrigo Adriano <rdgadriano@gmail.com>
2023-01-03 23:20:24 -0300

    Segunda levada

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 ///// DOM ////
 let input = document.querySelector('.textarea')
+let lupa = document.querySelector('.lupa')
+let resultado = document.querySelector('.resultado')
 
 let output = document.querySelector('.display');
 let btn1 = document.querySelector('#btn1');
@@ -9,7 +11,9 @@ let btn2 = document.querySelector('#btn2');
 
 
 function teste(){
-   return  output.innerHTML = input.value 
-}
 
+    lupa.style.display = 'none'
+    return  resultado.innerHTML = input.value 
+
+}
 
