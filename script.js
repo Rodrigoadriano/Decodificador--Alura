b3caff7154af9edc518dd52b830661ec3fdb330d
Rodrigo Adriano <rdgadriano@gmail.com>
2023-01-03 22:11:49 -0300

    Atuialização 03/01

---
+++ script.js
@@ -0,0 +1,15 @@
+///// DOM ////
+let input = document.querySelector('.textarea')
+
+let output = document.querySelector('.display');
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+
+
+
+
+function teste(){
+   return  output.innerHTML = input.value 
+}
+
+
