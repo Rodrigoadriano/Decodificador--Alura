374a583e3ba5e9e851f86d0f24065cf2bcb35eba
Rodrigo Adriano <rdgadriano@gmail.com>
2023-01-07 16:53:56 -0300

    adicionado função disabled no botão copiar

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let input = document.querySelector('.textarea')
 let lupa = document.querySelector('.lupa')
 let resultado = document.querySelector('.resultado')
 let conjunto = document.querySelector('.conjunto')
+let btn3 = document.querySelector('.btn3')
 
 function Criptografar(){
 
@@ -13,6 +14,8 @@ function Criptografar(){
     
     let tratado =  i.replaceAll("e","enter").replaceAll("i", "imes").replaceAll("a", "ai").replaceAll("o", "ober").replaceAll("u", "ufat")
     
+    btn3.innerHTML = "Copiar"
+    btn3.disabled = false
     
     return  resultado.innerHTML = tratado
 
@@ -26,14 +29,21 @@ function descriptografar(){
     
     let tratado =  i.replaceAll("enter","e").replaceAll("imes", "i").replaceAll("ai", "a").replaceAll("ober", "o").replaceAll("ufat", "u")
     
-    
+    btn3.innerHTML = "Copiar"
+    btn3.disabled = false
     return  resultado.innerHTML = tratado
 
 }
 
 function copiar(){
-    navigator.clipboard.writeText(resultado.innerHTML).then(()=> {alert("Mensagemn copiada")}).catch(() => {
-        alert("something went wrong");
+    navigator.clipboard.writeText(resultado.innerHTML)
+    .then(()=> {
+        alert("Mensagemn copiada")
+        input.value = null
+        btn3.innerHTML = "Copiado"
+        btn3.disabled = true
+    })
+    .catch(() => {alert("something went wrong");
       });
     
 }
